06bb48c327bc069f7da312372d69db66ccaf474a
Christos Kolonas <kolonaschr@gmail.com>
2025-07-07 18:24:56 +0300

    dto stuff

--- src/main/java/gr/alexc/otaobservatory/dto/UserModificationPermissionsRequestDTO.java
+++ src/main/java/gr/alexc/otaobservatory/dto/UserModificationPermissionsRequestDTO.java
@@ -1,11 +1,14 @@
 package gr.alexc.otaobservatory.dto;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class UserModificationPermissionsRequestDTO {
+    @NonNull
     private String email;
+    @NonNull
     private Boolean isAdmin;
 }
